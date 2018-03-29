4a494931b91957be0ae463d50e9a7bd5dbf7de1f
codeyu <codeyu@gmail.com>
2018-03-29 23:24:44 +0800

    up

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,5 +1,11 @@
 module.exports = function(robot) {
     robot.hear(/我是谁/, function(res){
-        res.send('世界上最帅的一个人');
-    });                       
+        res.send('是世界上最帅的一个人');
+    });  
+    robot.hear(/谁/, function(res){
+        res.send('你你你');
+    });   
+    robot.router.get('/', function(req, res) {
+        res.send('ok');
+    });                
 };
